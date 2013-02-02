55b4c523962a70b0f6fc842276c9a626099c3d70
Stephanie Yeung <shatt3redx@yahoo.com>
2013-02-02 14:49:46 -0500

    reset

--- htmlparser.js
+++ htmlparser.js
@@ -2,23 +2,23 @@ var getViewObject = function(json) {
     return JSON.parse(json.replace(/(\r\n|\n|\r)/gm,""));
 }
 
-var handleObjects = function(key, obj) {
-    var parseOut = [];
-    parseOut.push("<html>", "<style>");
-    parseOut.push("." + key + "{", "font: " + obj.fontSize + "px/" + obj.fontSize + "px '" + obj.font + "';");
-    parseOut.push("</style>", "</html>");
+var handleButton = function(key, button) {
+    var buttonOut = [];
+    buttonOut.push("<html>", "<style>");
+    buttonOut.push("." + key + " {", "font: " + button.fontSize + "px/" + button.fontSize + "px '" + button.font + "';", "}");
+    buttonOut.push("</style>", "</html>");
 
-    output.setValue(parseOut.join("\n"));
+    output.setValue(buttonOut.join("\n"));
 }
 
 var handleSubview = function(key, subview) {
     var cls = subview["class"];
-    handleObjects(key, subview);
+    handleButton(key, subview);
 }
 
 var parseCurrent = function() {
     var lines = editor.getValue();
-    var obj = getViewObject(lines)["MCNavigationView"];
+    var obj = getViewObject(lines)["MCNagivationView"];
     $(document).ready(function() {
         var html = $("#htmlout");
         var keys = Object.keys(obj);
