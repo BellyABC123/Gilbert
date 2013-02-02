f51340790f33c719f6906fefb06bfeee08a6c79b
Tyler Hedrick <tylerhedrick@me.com>
2013-02-02 17:49:15 -0500

    added window resizing

--- public/haterz.js
+++ public/haterz.js
@@ -1,13 +1,17 @@
-$("#editor").css("height", $(window).height()*.65);
-$("#canvas").css("height", $(window).height()-40);
-$("#output").css("height", $(window).height()*.30);
-$("#toggle-output").click(function(){
-    if ($("#htmlout").is(":visible")) {
-        $("#htmlout").hide();
-        $("#iosout").show();
-    }
-    else {
-        $("#iosout").hide();
-        $("#htmlout").show();
-    }
-  });
+function stopDemHaterz() {
+	$("#editor").css("height", $(window).height()*.65);
+	$("#editor").css("width", $(window).width() - 400);
+	$("#mobileView").css("height", $(window).height()-40);
+	$("#output").css("height", $(window).height()*.30);
+	$("#output").css("width", $(window).width() - 400);
+	$("#toggle-output").click(function(){
+	    if ($("#htmlout").is(":visible")) {
+	        $("#htmlout").hide();
+	        $("#iosout").show();
+	    }
+	    else {
+	        $("#iosout").hide();
+	        $("#htmlout").show();
+	    }
+	  });
+}
