20b63394c8ffa0089d7988f892c9ff91bb97855d
Tyler Hedrick <tylerhedrick@me.com>
2013-02-02 14:22:16 -0500

    adding local version of ace

--- public/haterz.js
+++ public/haterz.js
@@ -1,7 +1,6 @@
 $("#editor").css("height", $(window).height()*.65);
 $("#canvas").css("height", $(window).height()-40);
 $("#output").css("height", $(window).height()*.30);
-$("#output").css("margin-top", $(window).height()*.65);
 
 $("#toggle-output").click(function(){
     if ($("#htmlout").is(":visible")) {
